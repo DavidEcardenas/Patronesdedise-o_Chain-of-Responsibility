6c59641f325d36b74308f35a1e15840dd7121b78
DavidECardenas <agudelodavid88@gmail.com>
2025-09-09 14:13:33 -0500

    se modifica el App.java que teniamos inicialmente

--- maquinadispensadora/src/App.java
+++ maquinadispensadora/src/App.java
@@ -1,5 +1,33 @@
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        
+        String input = JOptionPane.showInputDialog("ingrese la cantidad que sera dispensada");
+        int cantidad = Integer.parseInt(input);
+
+        if (cantidad % valor.CincoMil.getValor() != 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Recuerde que la cantidad debe ser multiplo de 5000 usted puso: " + cantidad);
+            return;
+        }
+     
+
+        Manejador v100 = new V100k();
+        Manejador v50 = new V50k();
+        Manejador v20 = new V20k();
+        Manejador v10 = new V10k();
+        Manejador v5 = new V5k();
+
+        v100.setSiguiente(v50);
+        v50.setSiguiente(v20);
+        v20.setSiguiente(v10);
+        v10.setSiguiente(v5);
+
+        v100.procesar(cantidad);
+
+         
     }
+    
 }
